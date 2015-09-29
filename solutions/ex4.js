3ad4e4e86f7c9e38c0270d7d0c8be631bc045636
Sebastian Golaszewski (taagose1) <Sebastian.Golaszewski@swisscom.com>
2015-09-29 10:52:06 +0200

    adapted namespace pattern for ex4

--- solutions/ex4.js
+++ solutions/ex4.js
@@ -7,28 +7,28 @@
     "use strict";
 
     // TODO: Exercise 4.1 - Bind the function 'fToBind' to the given 'context'.
-    global.bindIt = function (fToBind, context) {
+    function bindIt(fToBind, context) {
         return fToBind.bind(context);
-    };
+    }
 
     // TODO: Exercise 4.2 - Create a constuctor function that takes a 'name' argument.
-    //                Note: Remember to use an upper-case name for the cunstructor.
+    //                Note: Remember to use an upper-case name for the constructor.
     //                Note: First create a constructor without any properties
     //                Note: Properties are set on 'this'.
     //                Note: The prototype is inherited from <constructor-fn>.prototype.
-    global.createIt = function (prototype) {
+    function createIt(prototype) {
         var Constructor = function Constructor(name) {
             this.name = name;
         };
         Constructor.prototype = prototype;
 
         return Constructor;
-    };
+    }
 
     // TODO: Exercise 4.3 (optional) - Implement your own version of 'bind'.
     //                Note: Use the tests to guide you.
     //                Note: Good luck!
-    global.myBind = function myBind(fToBind, context) {
+    function myBind(fToBind, context) {
         var fBound = function () {
             fToBind.call(context);
         };
@@ -36,16 +36,22 @@
         fBound.prototype = fToBind.prototype;
 
         return fBound;
-    };
+    }
 
     // TODO: Exercise 4.3 (optional) - Implement your own version of Object.create().
     //                Note: Use the tests to guide you.
     //                Note: Good luck!
-    global.myObjectCreate = function myObjectCreate(context) {
+    function myObjectCreate(context) {
         var fNOP = function () {
         };
         fNOP.prototype = context;
 
         return new fNOP();
-    };
+    }
+
+    global.ex4 = global.ex4 || {};
+    global.ex4.bindIt = global.ex4.bindIt || bindIt;
+    global.ex4.createIt = global.ex4.createIt || createIt;
+    global.ex4.myBind = global.ex4.myBind || myBind;
+    global.ex4.myObjectCreate = global.ex4.myObjectCreate || myObjectCreate;
 })(window);
