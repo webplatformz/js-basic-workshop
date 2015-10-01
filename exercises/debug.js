fe008ed2cfa8789bdcddafe0d5d47fdba7b229b3
Sebastian Golaszewski (taagose1) <Sebastian.Golaszewski@swisscom.com>
2015-10-01 14:49:23 +0200

    adapted debug example

--- exercises/debug.js
+++ exercises/debug.js
@@ -1,9 +1,11 @@
 (function($) {
     $(function() {
         console.log("Lorem ipsum dolor sit");
+        var x = "Hello World";
         debugger;
         console.log("amet, consetetur sadipscing");
         console.log("elitr, sed diam nonumy");
+        x = "Hey Ho!";
         console.log("eirmod tempor invidunt");
         console.log("ut labore et dolore");
     })
